900f0885bb5f70d0e24690352a15ada14a20d1f3
crlxs <belmontecarles@gmail.com>
2024-05-21 15:22:32 +0200

    Renamed files

--- setup.sh
+++ setup.sh
@@ -1,71 +1,28 @@
 #!/bin/bash
 
-### Run without sudo, otherwise it
+### Options and variables ###
 
-### Also automate git setup? https://kbroman.org/github_tutorial/pages/first_time.html
-### How to maintain dwm config and customization in git https://dwm.suckless.org/customisation/patches_in_git/
+dwm_dependencies="xorg xserver-xorg build-essential libx11-dev libxft-dev libxinerama-dev"
+my_packages="curl git zsh neovim chromium compton fonts-noto-color-emoji nmap net-tools"
 
-### OPTIONS AND VARS ###
-suckless_dirs=(~/.local/src/dwm ~/.local/src/dmenu ~/.local/src/st)
-
-# Update, upgrade and install dwm/dmenu/st dependencies and other
-sudo apt update && sudo apt upgrade -y && sudo apt install -y xorg xserver-xorg build-essential libx11-dev libxft-dev libxinerama-dev compton git
-#sudo apt install -y neovim curl chromium nmap net-tools
-
-# Create necessary directories
-mkdir -p ~/.local/src ~/.config
-
-# Clone suckless software repos
-git clone git://git.suckless.org/dwm ~/.local/src/dwm
-git clone git://git.suckless.org/st ~/.local/src/st
-git clone git://git.suckless.org/dmenu ~/.local/src/dmenu
-
-# Create patches directories
-mkdir ~/.local/src/dwm/patches ~/.local/src/dmenu/patches ~/.local/src/st/patches
-
-# Download dwm patches
-wget https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-6.4.diff -P ~/.local/src/dwm/patches
-wget https://dwm.suckless.org/patches/alpha/dwm-alpha-20230401-348f655.diff -P ~/.local/src/dwm/patches
-wget https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff -P ~/.local/src/dwm/patches
-
-# Download st patches
-wget https://st.suckless.org/patches/alpha/st-alpha-20220206-0.8.5.diff -P ~/.local/src/st/patches
-wget https://st.suckless.org/patches/delkey/st-delkey-20201112-4ef0cbd.diff -P ~/.local/src/st/patches
-
-# Download dmenu patches
-#wget https://tools.suckless.org/dmenu/patches/alpha/dmenu-alpha-20230110-5.2.diff -P ~/.local/src/dmenu/patches
-#wget https://tools.suckless.org/dmenu/patches/border/dmenu-border-20201112-1a13d04.diff -P ~/.local/src/dmenu/patches
-#wget https://tools.suckless.org/dmenu/patches/center/dmenu-center-5.2.diff -P ~/.local/src/dmenu/patches
-
-# Function to apply patches
-apply_patches() {
-    local src_dir=$1
-    cd $src_dir || exit 1
-    for patch in patches/*.diff; do
-        patch -p1 < "$patch"
-    done
+### Functions ###
+git_setup () {
+	git config --global user.name "crlxs"
+	git config --global user.email "belmontecarles@gmail.com"
+	
+	ssh-keygen -t rsa -C "belmontecarles@gmail.com"
+	cat ~/.ssh/id_rsa.pub
 }
 
-# Apply patches to dwm, dmenu and st
-apply_patches ~/.local/src/dwm
-apply_patches ~/.local/src/st
-#apply_patches ~/.local/src/dmenu
 
-# Function to build and install suckless software
-make_install() {
-	local src_dir=$1
-	cd $src_dir || exit 1
-	make
-	sudo make install
-}
+### The actual script ###
 
-# Make and install each suckless software
-for dir in "${suckless_dirs[@]}"; do
-    make_install "$dir"
-done
+# Update, upgrade and install
+sudo apt update && sudo apt upgrade -y && sudo apt install -y $dwm_dependencies $my_packages
 
-echo "Initial setup complete. Create a ~/.xinitrc file and launch dwm with startx."
+# Create ~/ directories
+mkdir -p ~/.config ~/.local/src ~/.local/share ~/.local/bin
 
-######
-### dmenu patches need work
-######
+# Clone my forks of dwm, dmenu and st
+git clone https://github.com/crlxs/dwm.git ~/.local/src/dwm
+
