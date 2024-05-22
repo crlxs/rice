0ac6fb195296681921c15dc885ada0a1a867cb98
crlxs <belmontecarles@gmail.com>
2024-05-22 17:55:45 +0200

    Final version

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+#1. Run setup.sh
+#2. Configure git with ssh (https://kbroman.org/github_tutorial/pages/first_time.html). Prompt user to add key to github.com
+#3. Clone dotfiles repo. (https://www.atlassian.com/git/tutorials/dotfiles)
+#4. Set upstream for repo.
+
+
 #!/bin/bash
 
 ### Options and variables ###
@@ -10,11 +16,21 @@ suckless_dirs=($USER_HOME/.local/src/dwm $USER_HOME/.local/src/dmenu $USER_HOME/
 
 ### Functions ###
 git_setup () {
-	git config --global user.name "crlxs"
-	git config --global user.email "belmontecarles@gmail.com"
-	
-	ssh-keygen -t rsa -C "belmontecarles@gmail.com"
-	cat ~/.ssh/id_rsa.pub
+        git config --global user.name "crlxs"
+        git config --global user.email "belmontecarles@gmail.com"
+
+        ssh-keygen -t rsa -C "belmontecarles@gmail.com"
+        cat ~/.ssh/id_rsa.pub
+		# Warning
+		echo -e "\033[0;31mADD THE SSH KEY TO GITHUB.COM BEFORE PROCEDING\033[0m"
+		# Pause the script and wait for user input
+		echo "Press any key to continue..."
+		read -n 1 -s
+		# Continue with the rest of your script
+		echo "Continuing after user input..."
+		# Verify
+		ssh -T git@github.com
+		
 }
 
 make_install() {
@@ -29,8 +45,8 @@ make_install() {
 # Update, upgrade and install
 sudo apt update && sudo apt upgrade -y && sudo apt install -y $dwm_dependencies $my_packages
 
-# Create ~/ directories
-mkdir -p $USER_HOME/.config $USER_HOME/.local/src $USER_HOME/.local/share $USER_HOME/.local/bin
+# Create ~/ directories, $USER_HOME/.dotfiles is the bare git repo dir.
+mkdir -p $USER_HOME/.dotfiles $USER_HOME/.config $USER_HOME/.local/src $USER_HOME/.local/share $USER_HOME/.local/bin
 
 # Clone my forks of dwm, dmenu and st
 git clone https://github.com/crlxs/dwm $USER_HOME/.local/src/dwm
@@ -39,12 +55,33 @@ git clone https://github.com/crlxs/st $USER_HOME/.local/src/st
 
 # Make and install each suckless software
 for dir in "${suckless_dirs[@]}"; do
-	make_install "$dir"
+        make_install "$dir"
 done
 
-# Change default shell to zsh, copy basic .zshrc and add path for ~/.local/bin
+# Change default shell to zsh
 chsh -s $(which zsh)
-cp /etc/zsh/newuser.zshrc.recommended $USER_HOME/.zshrc
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> $USER_HOME/.zshrc
 
-echo "Initial setup complete. Create a ~/.xinitrc file and launch dwm with startx."
+# Setup git ssh
+git_setup
+
+# clone your dotfiles repo
+git clone --bare git@github.com:crlxs/dotfilestest $HOME/.dotfiles
+
+# Define alias for current shell
+#alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# Checkout the actual contet from the bare git repo into $HOME
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+# If any error appear regarding untracked files, delete them.
+
+# Set the flag showUntrackedFiles to no on this specific (local) repository:
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
+# Set upstream
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME push --set-upstream origin master
+
+# Pull
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull
+
+# Rm old files in ~/
+rm $HOME/.bash* $HOME/.profile
